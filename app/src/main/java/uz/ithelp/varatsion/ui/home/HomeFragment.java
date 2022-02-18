95aaa66c3eb8b3aee0bee712164d0088365ad278
shoxumarzoda <shoxumarzoda@gmail.com>
2022-02-18 21:02:25 +0500

    yangi maruza mavzulari yuklandi

--- app/src/main/java/uz/ithelp/varatsion/ui/home/HomeFragment.java
+++ app/src/main/java/uz/ithelp/varatsion/ui/home/HomeFragment.java
@@ -55,53 +55,60 @@ public class HomeFragment extends Fragment {
     private void gridListData() {
         modelArrayList = new ArrayList<Model>();
 
-        modelArrayList.add(new Model("1-ma’ruza. Variatsion hisobning asosiy masalasini qo‘yilishi. Funksionalning kuchli va kuchsiz minimumlari. Joiz egri chiziqlar.",
+        modelArrayList.add(new Model("1-ma’ruza. Variatsion hisobning predmeti, funksionalning ekstremumi.",
                 "file:///android_asset/m2.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("2-ma’ruza. Variatsiya usulining mohiyati. Egri chiziqlarning yaqinligi. Egri chiziq va funksional variatsiyalari. Birinchi va ikkinchi variatsiyalar. Variatsiyalarda kuchsiz minimumning zaruriy  sharti.",
+        modelArrayList.add(new Model("2-ma’ruza. Variatsion hisobning asosiy masalasi (birinchi variatsiyani tekshirish)",
                 "file:///android_asset/m1.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("3-mavzu. Lagranj lemmasi. Eyler-Lagranj tenglamasi. Eyler tenglamasining ekstremali, uning integrallanuvchi hollari. Dyu-Bua-Reymon va Eyler tenglamasining qat’iy chiqarilishi",
+        modelArrayList.add(new Model("3-mavzu. Variatsion hisobning asosiy masalasi (ikkinchi variatsiyani tekshirish, ekstremumning yetarli shartlari).",
                 "file:///android_asset/m3.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("4-mavzu. Ikkinchi tartibli zaruriy shartlar. Gilbert sharti. Nomaxsus ekstremal tushunchasi.",
+        modelArrayList.add(new Model("4-mavzu. Variatsion hisob asosiy masalasining ba’zi umumlashmalari",
                 "file:///android_asset/m4.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("5-mavzu. Gilbert teoremasi. Bo‘lakli- silliq funksiyalar uchun funksionalning minimumi sharti. Masalaning qo‘yilishi. Veyyershtass-Erdmon sharti. Lejandr sharti. Yakobi sharti. Yakobi differensial tenglamasi. Kuchsiz minimumning Yakobi zaruriy sharti.",
+        modelArrayList.add(new Model("5-mavzu. Shartli ekstrеmumga qo`yilgan variatsion masalalar",
                 "file:///android_asset/m5.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("6-mavzu. n ta funksiyaga bog‘liq bo‘lgan funksionallar uchun kuchsiz minimumning zaruriy sharti. Yuqori tartibli hosilalarga bog‘liq bo‘lgan funksional uchun kuchsiz minimumning zaruriy sharti.",
+        modelArrayList.add(new Model("6-mavzu. Izoperimetrik masalalar",
                 "file:///android_asset/m6.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("7-mavzu. Eyler – Puasson tenglamasi. n ta erkin o‘zgaruvchili funksiyalarga bog‘liq bo‘lgan funksional uchun kuchsiz minimumning zaruriy sharti.",
+        modelArrayList.add(new Model("7-mavzu. Optimal boshqaruv masalasining qo’yilishi. Pontryaginning maksimum prinsipi.",
                 "file:///android_asset/m7.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("8-mavzu. Eyler-Lagranj tenglamasining umumlashmasi. Parametrik ko‘rinishdagi variatsion masalalar. Yakobi tenglamasi. Veyyershtrass funksiyasi. Kuchsiz minimumning Lejandr sharti.",
+        modelArrayList.add(new Model("8-mavzu. Terminal boshqarish masalasi uchun maksimum prinsipi.",
                 "file:///android_asset/m8.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("9-ma’ruza. Ko‘p o‘zgaruvchili funksiyalarning ekstremumlari.  Zaruriy va yetarli shartlar. Shartli ekstremum masalasi.  Lagranjning aniqmas ko‘paytuvchilar usuli.",
+        modelArrayList.add(new Model("9-mavzu. Chiziqli boshqarish sistemalari. Tez harakat masalalari.",
                 "file:///android_asset/m9.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("10 – ma’ruza.  Boshqariluvchi obyektlar. L.S. Pontryagining maksimum prinsipi. Joiz boshqaruvlar. Tezkor masalalar uchun maksimum prinsipi.",
+        modelArrayList.add(new Model("10-ma’ruza. Chekli o’lchovli ekstremal masalalarning umumiy qo’yilishi, xossalari va sinflari. Ekstremal masala yechimining mavjudligi.",
                 "file:///android_asset/m10.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("11– ma’ruza Dinamik dasturlash usuli. Bellman funksiyasi va differensiallanuvchiligi.  Maksimum prinsipi bilan Lagranjning usuli orasidagi bog‘liqlik.",
+        modelArrayList.add(new Model("11 – ma’ruza Qavariq  to’plamlar  va ular ustida amallar.Qavariq to’plamlarning ajralishi. ",
                 "file:///android_asset/m11.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("12 - ma’ruza. Bellman tenglamasi. Optimal boshqarishning sintezi masalasi. Sodda apiyeriodik va tebranma sistemalar uchun tezkor boshqarishni sintez qilish masalasi.",
+        modelArrayList.add(new Model("12 – ma’ruza Qavariq funksiyalar va ularning xossalari. Funksiyalarning qavariqlik kriteriyalari. Qavariq funksiyalarning ekstremumlari.",
                 "file:///android_asset/m12.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("13-ma’ruza Tezkor boshqarishni sintez qilish masalasi uchun Bellman funksiyasini qurish va tekshirish.",
+        modelArrayList.add(new Model("13 - ma’ruza Umumiy minimallashtirish masalasi uchun optimallik shartlari",
                 "file:///android_asset/m13.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("14-ma’ruza. Optimal boshqarishning chiziqli masalasi va uning qo‘yilishi.",
+        modelArrayList.add(new Model("14-ma’ruza. Shartsiz va shartli ekstremum masalalari.",
                 "file:///android_asset/m14.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("15-MA’RUZA Maksimum prinsipining soddalashishi. Erishishlik sferasi va uning qavariqligi. Maksimum prinsipining isboti.",
+        modelArrayList.add(new Model("15-ma’ruza. Chiziqli bo”lmagan programmalashtirishning umumiy masalasi",
                 "file:///android_asset/m15.htm", R.drawable.ic_baseline_menu_book_24w));
 
-        modelArrayList.add(new Model("16 – ma’ruza Umumiy holatda bo‘lish sharti va maksimum prinsipi yetarli shart sifatida.",
+        modelArrayList.add(new Model("16-MA’RUZA Qavariq programmalashtirish masalasi",
                 "file:///android_asset/m16.htm", R.drawable.ic_baseline_menu_book_24w));
+
+        modelArrayList.add(new Model("17 – ma’ruza Matematik programmalashtirishning hisoblash usullari",
+                "file:///android_asset/m17.htm", R.drawable.ic_baseline_menu_book_24w));
+
+        modelArrayList.add(new Model("18 ma’ruza Chiziqli programmalashtirish masalasi",
+                "file:///android_asset/m18.htm", R.drawable.ic_baseline_menu_book_24w));
+
 
     }
 
